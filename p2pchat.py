4e93754e407c2bede06395fc79b3a056dc146cc6
Christopher Paul <60367876+paulchr94@users.noreply.github.com>
2025-08-04 01:36:34 -0400

    Update p2pchat.py
    Setup the Server listening for a connection

--- p2pchat.py
+++ p2pchat.py
@@ -4,3 +4,14 @@ HOST = '127.0.0.1'  # change to peer's IP
 PORT = 12345        # any unused port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def start_server():
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    print(f"Connected by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print("Peer:", data.decode())
