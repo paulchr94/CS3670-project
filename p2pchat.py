07416eb6996ef7dc25d88006f13b66c438f20156
Christopher Paul <60367876+paulchr94@users.noreply.github.com>
2025-08-04 01:37:51 -0400

    Update p2pchat.py
    Create a system to handle threading.

--- p2pchat.py
+++ p2pchat.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'  # change to peer's IP
 PORT = 12345        # any unused port
@@ -22,3 +23,11 @@ def start_client():
     while True:
         msg = input("You: ")
         s.sendall(msg.encode())
+
+
+
+server_thread = threading.Thread(target=start_server)
+client_thread = threading.Thread(target=start_client)
+
+server_thread.start()
+client_thread.start()
